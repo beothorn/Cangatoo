714b87d0fd506d9ed00e2c497225e64f399b343c
lucas <lucass@gmail.com>
2011-09-01 01:47:26 -0300

    Added very simple unit testing

---
+++ testMainElement.js
@@ -0,0 +1,8 @@
+function TestMainElement(){
+	this.testSimpleCollision = function(){
+		assertTrue(true,"testSimpleCollision - Should be true");
+	}
+	this.testSimpleCollision2 = function(){
+		assertTrue(false,"testSimpleCollision - Should be true");
+	}
+}
