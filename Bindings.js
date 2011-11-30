332149a91772e2d0e76710c2268ce47c4e290358
Lucas Santos <lcsantos@objective.com.br>
2011-11-30 18:12:00 -0200

    #40 Remove touch buttons

--- Bindings.js
+++ Bindings.js
@@ -2,7 +2,6 @@ function Bindings(){
 	
 	this.doAllBindings = function(){
 		this.bindMenuEvents();
-		this.bindTouchKeys();
 		this.bindGameEditor();
 		this.bindHideLinks();
 	}
@@ -39,20 +38,6 @@ function Bindings(){
 				$("#viewEditor").text(checkedShowString);
 			}
 		});
-		
-		$("#viewTouchButtons").click(function(event){
-				var checkedShowString =  String.fromCharCode(10003)+" Touch buttons";
-				var uncheckedShowString =  "\u00A0\u00A0 Touch buttons";
-				
-				event.preventDefault();
-				if($("#viewTouchButtons").text() == checkedShowString){
-					$("#touchKeyboard").hide("slow");
-					$("#viewTouchButtons").text(uncheckedShowString);
-				}else{
-					$("#touchKeyboard").show("slow");
-					$("#viewTouchButtons").text(checkedShowString);
-				}
-		});
 	
 		$("#gameRestartLevel").click(function(event){
 				editMode = false;
@@ -82,40 +67,6 @@ function Bindings(){
 		$("#helpAbout").click(function(event){
 				event.preventDefault();
 				$("#helpAboutText").show("slow");
-		});
-	};
-	
-	this.bindTouchKeys = function(){
-	  $("#upBtn").mouseover(function(event){
-	  		keyDown(up);
-		});
-			
-		$("#upBtn").mouseout(function(event){
-			keyUp(up);
-		});
-		
-		$("#downBtn").mouseover(function(event){
-			keyDown(down);
-		});
-			
-		$("#downBtn").mouseout(function(event){
-			keyUp(down);
-		});
-		
-		$("#leftBtn").mouseover(function(event){
-			keyDown(left);
-		});
-			
-		$("#leftBtn").mouseout(function(event){
-			keyUp(left);
-		});
-		
-		$("#rightBtn").mouseover(function(event){
-			keyDown(right);
-		});
-			
-		$("#rightBtn").mouseout(function(event){
-			keyUp(right);
 		});
 	};
 	
